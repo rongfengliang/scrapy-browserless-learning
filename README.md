5f610eecf29ca2f7b5d9cca04c306bf3010f056f
rongfengliang <1141591465@qq.com>
2024-06-03 07:42:29 +0800

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# scrapy minio feed exports
+
+## How to Running
+
+* init venv
+
+```code
+python -m venv venv
+source venv/bin/activate
+```
+
+* running
+
+```code
+pip install requirements.txt
+
+cd dalongdemo
+
+scrapy crawl blogs
+
+```
+
---
+++ dalongdemo/dalongdemo/spiders/blogs.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+import scrapy
+
+import json
+
+from dalongdemo.items import DalongdemoItem
+
+
+class BlogsSpider(scrapy.Spider):
+    name = "blogs"
+
+    def start_requests(self):
+
+        urls = [
+            "https://cnblogs.com/rongfengliang"
+        ]
+
+        for url in urls:
+            options = {
+                "url": url,
+                "gotoOptions": {
+                    "waitUntil": "networkidle0"
+                }
+            }
+            yield scrapy.Request(
+                url="http://localhost:3000/content?token=6R0W53R135510",
+                method='POST',
+                dont_filter=True,
+                headers={"Content-Type": "application/json"},
+                body=json.dumps(options),
+                callback=self.parse
+            )
+
+    def parse(self, response):
+        for item in response.css("div.post"):
+            dalongItem = DalongdemoItem()
+            dalongItem["title"] = item.css("div.post .postTitle2 > span").get()
+            dalongItem["content"] = item.css("div.postbody .c_b_p_desc").get()
+            yield dalongItem
